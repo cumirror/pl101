35dfe8987201ef951f3fd2d5e9e0f8a9232b2c17
cumirror <tongjinam@qq.com>
2013-06-20 12:34:43 +0800

    PEG for expression

---
+++ peg_test.js
@@ -0,0 +1,17 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('express.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+// Do a test
+console.log( parse("'  (a b c  ( e f\r\n g ) ) ") );
+console.log( parse("'\t a") );
+console.log( parse("'(a b c) ") );
+//assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+
+
