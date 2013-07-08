3437079aa67c1dc357332c881198d6f1a94d604d
cumirror <tongjinam@qq.com>
2013-07-08 21:25:15 +0800

    updata scheem files, use: pegjs -e SCHEEM *.peg *.js for generate parser

--- scheem_express_test.js
+++ scheem_express_test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var fs = require('fs'); // for loading files
 
 // Read file contents
-var data = fs.readFileSync('express.peg', 'utf-8');
+var data = fs.readFileSync('scheem_express.peg', 'utf-8');
 // Show the PEG grammar file
 console.log(data);
 // Create my parser
---
+++ scheem_i.js
@@ -0,0 +1,81 @@
+// A half-baked implementation of evalScheem
+var evalScheem = function (expr, env) {
+    var ret = [];
+    var i = 0;
+ 
+    // Numbers evaluate to themselves
+    if (typeof expr === 'number') {
+        return expr;
+    }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+    }
+    // Look at head of list for operation
+    switch (expr[0]) {
+    case '+':
+        return evalScheem(expr[1], env) + evalScheem(expr[2], env);
+    case '-':
+        return evalScheem(expr[1], env) - evalScheem(expr[2], env);
+    case '*':
+        return evalScheem(expr[1], env) * evalScheem(expr[2], env);
+    case '/':
+        return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+    case '<':
+        if(evalScheem(expr[1], env) < evalScheem(expr[2], env))
+            return '#t';
+        else
+            return '#f';
+    case '>':
+	if(evalScheem(expr[1], env) > evalScheem(expr[2], env))
+            return '#t';
+	else
+            return '#f';
+    case '<':
+	if(evalScheem(expr[1], env) < evalScheem(expr[2], env))
+	    return '#t';
+	else
+	    return '#f';
+    case '=':
+        if(evalScheem(expr[1], env) === evalScheem(expr[2], env))
+            return '#t';
+        else
+            return '#f';
+    case 'if':
+        if(evalScheem(expr[1], env) === '#t')
+            return evalScheem(expr[2], env);            
+        else 
+	    return evalScheem(expr[3], env);
+    case 'set!':
+    case 'define':
+        env[evalScheem(expr[1], env)] = evalScheem(expr[2], env);
+        return 0;
+    case 'begin':
+        i = 1;
+        while(i < expr.length){
+            ret = evalScheem(expr[i], env);
+            i++;
+        }
+        return ret;
+    case 'quote':
+        return expr[1];
+    case 'cons':
+	ret[ret.length] = evalScheem(expr[1], env);
+	evalScheem(expr[2], env).forEach(function(item,index,array){
+            ret[ret.length] = item;
+	});
+	return ret;
+    case 'car':
+	return evalScheem(expr[1], env)[0];
+    case 'cdr':
+	i = 1;
+	var list = evalScheem(expr[1], env); 
+	while(i < list.length){
+            ret[i-1] = list[i];
+            i++;
+	}
+	return ret;
+    }
+};
+
+//exports.evalScheem = evalScheem;
